eafb4cbb10bf8f31f532d455a273c461aebb1e6b
Nicholas Jarretta <nicholas@jarretta.com>
2024-03-24 16:51:52 +0000

    Fix quotes and DB value in the tag script

--- fzf-taskman-query.sh
+++ fzf-taskman-query.sh
@@ -7,4 +7,4 @@ db=$1
 
 IFS=$'\n\t'
 
-$taskman_dir/taskman-query.sh $1 | fzf --preview $'echo {} | awk -F\' \' \'{print $1}\' | xargs $taskman_dir/taskman-desc.sh taskman.db'
+$taskman_dir/taskman-query.sh $1 | fzf --preview "echo {} | awk -F' ' '{print \$1}' | xargs $taskman_dir/taskman-desc.sh $1"
